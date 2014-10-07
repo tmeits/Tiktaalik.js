3479f5efbb10ec1fa2cefde99d816a1e499128f3
iva <ilynva@gmail.com>
2014-10-08 00:06:56 +0900

    new SampleDriver.sh

--- source/SampleDriver.sh
+++ source/SampleDriver.sh
@@ -1,8 +1,10 @@
 #
 cd ~/git/Tiktaalik/source
 rm *.sym
+rm *.o
 ~/xds/bin/xc =compile Random.ob2
-~/xds/bin/xc =complie Controls.ob2
+~/xds/bin/xc =compile Controls.ob2
 ~/xds/bin/xc =compile TestFunctions.ob2
 ~/xds/bin/xc =compile Tiktaalik.ob2
-~/xds/bin/xc =make Tiktaalik.ob2 +MAIN
+~/xds/bin/xc =make SampleDriver.ob2 +MAIN
+
